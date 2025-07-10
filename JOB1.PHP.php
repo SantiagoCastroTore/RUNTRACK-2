e82dd381ffdedf6d490eb776667db6193fa84274
SantiagoCastroTore <santiago.castro@laplateforme.io>
2025-07-10 14:01:18 +0200

    JOB 1

---
+++ JOB1.PHP.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$str= " LaPlateforme";
+$str2= " Vive";
+$str3= "!";
+$val= "4";
+
+echo "Bienvenue  $str2  $str $str3" ;
+<br>
+
+$val +=4;
+echo   $val ;
+?> 
+</body>
+</html>
